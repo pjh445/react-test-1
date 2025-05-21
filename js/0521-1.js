087db086e690fd36896cfc7a514c21bd3d5a406b
jeonghwa,park <pjh445@gmail.com>
2025-05-21 12:03:25 +0900

    Add files via upload

---
+++ js/0521-1.js
@@ -0,0 +1,12 @@
+//가독성과 표준명명 관례를 고려하여, i 대신에 props라고 표기한다.
+function Mall ( ) {
+    return <b>코엑스 몰 점</b>;
+};
+function Saturday( { shop } ) {
+    return (
+        <h3>친구들과 { shop } <Mall />에서 쇼핑도하고 식사도 하며 즐거운 시간을 보냈습니다.</h3>
+    );
+}
+
+const root = ReactDOM.createRoot( document.getElementById('root') );
+root.render( <Saturday shop="스타필드" /> );
